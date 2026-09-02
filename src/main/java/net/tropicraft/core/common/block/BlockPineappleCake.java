287259666a1ca95f4de32c0fd82d905f6037ef36
FlashyGuitar603 <cioveierobert@gmail.com>
2026-09-02 03:21:54 -0700

    Fixed pineapple cake block being associated with the vanilla cake, thus creating problems when middle mouse clicking on the block or when waila tried to get the item for said block.

--- src/main/java/net/tropicraft/core/common/block/BlockPineappleCake.java
+++ src/main/java/net/tropicraft/core/common/block/BlockPineappleCake.java
@@ -2,6 +2,17 @@ package net.tropicraft.core.common.block;
 
 import net.minecraft.block.BlockCake;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import java.util.Random;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockPineappleCake extends BlockCake {
 
@@ -11,5 +22,10 @@ public class BlockPineappleCake extends BlockCake {
         this.setDefaultState(this.blockState.getBaseState().withProperty(BITES, Integer.valueOf(0)));
         this.setTickRandomly(true);
         this.setSoundType(SoundType.CLOTH);
+    }
+
+    @Override
+    public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
+        return new ItemStack(Item.getItemFromBlock(this));
     }
 }
